0e1b1b4c81dfdcb730b071ae88eda9ff65cca25e
oihid <c.ouchene@esi-sba.dz>
2023-03-20 22:52:08 +0100

    fixeeeed

--- ShopApp/models.py
+++ ShopApp/models.py
@@ -2,7 +2,7 @@ from django.core.validators import MinValueValidator
 from django.db import models
 
 ####
-
+###########################
 class Promotion(models.Model):
     description = models.CharField(max_length=255)
     discount = models.FloatField()
