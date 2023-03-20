0edb2593a43db95418eed31c713ae6319cfaab71
oihid <c.ouchene@esi-sba.dz>
2023-03-20 22:39:48 +0100

    git ignore

--- FamilyShop/settings.py
+++ FamilyShop/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
+#test
 # Application definition
 
 INSTALLED_APPS = [
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'ShopApp'
+    'ShopApp',
 ]
 
 MIDDLEWARE = [
