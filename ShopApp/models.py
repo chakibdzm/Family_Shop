0da69ddb323648996e8492a9d235fd02951566e9
SadekMaachou <ys.maachou@esi-sba.dz>
2023-03-22 13:42:40 +0100

    adding the db

--- ShopApp/models.py
+++ ShopApp/models.py
@@ -21,7 +21,7 @@ class Collection(models.Model):
 
 
 class Product(models.Model):
-    id=models.UUIDField(default=uuid4, unique=True,editable=False,primary_key=True)
+    id = models.UUIDField(default=uuid4, unique=True,editable=False,primary_key=True)
     title = models.CharField(max_length=255)
     slug = models.SlugField()
     description = models.TextField(null=True, blank=True)
