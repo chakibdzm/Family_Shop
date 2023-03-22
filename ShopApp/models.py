f2a2f5f6973da1fb7a23062bfdc0ea5720a28908
LydiaDelulu <i.khelif@esi-sba.dz>
2023-03-22 22:49:42 +0100

    models

--- ShopApp/models.py
+++ ShopApp/models.py
@@ -1,6 +1,7 @@
 from django.core.validators import MinValueValidator
 from django.db import models
 from uuid import uuid4
+from django.contrib.auth.models import User
 
 
 class Promotion(models.Model):
@@ -8,62 +9,77 @@ class Promotion(models.Model):
     discount = models.FloatField()
 
 
-class Collection(models.Model):
-    title = models.CharField(max_length=255)
-    featured_product = models.ForeignKey(
-        'Product', on_delete=models.SET_NULL, null=True, related_name='+', blank=True)
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255,unique=True)
+    parent_category = models.ManyToManyField('self', symmetrical=False, on_delete=models.CASCADE)
 
     def __str__(self) -> str:
-        return self.title
+        return self.name
 
     class Meta:
-        ordering = ['title']
+        ordering = ['name']
 
 
 class Product(models.Model):
-    title = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
     slug = models.SlugField()
-    description = models.TextField(null=True, blank=True)
-    unit_price = models.DecimalField(
+    description = models.TextField()
+    image = models.ImageField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    price = models.DecimalField(
         max_digits=6,
         decimal_places=2,
         validators=[MinValueValidator(1)])
     inventory = models.IntegerField(validators=[MinValueValidator(0)])
     last_update = models.DateTimeField(auto_now=True)
-    collection = models.ForeignKey(Collection, on_delete=models.PROTECT, related_name='products')
     promotions = models.ManyToManyField(Promotion, blank=True)
 
     def __str__(self) -> str:
-        return self.title
+        return self.name
 
     class Meta:
-        ordering = ['title']
+        ordering = ['name']
+
+class Product_variation(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    description = models.TextField(max_length=255)
+    image = models.ImageField()
+    price = models.DecimalField(null=True)
 
 
-class Customer(models.Model):
-    MEMBERSHIP_BRONZE = 'B'
-    MEMBERSHIP_SILVER = 'S'
-    MEMBERSHIP_GOLD = 'G'
+class User(models.Model):
+   # MEMBERSHIP_BRONZE = 'B'
+    #MEMBERSHIP_SILVER = 'S'
+    #MEMBERSHIP_GOLD = 'G'
 
-    MEMBERSHIP_CHOICES = [
-        (MEMBERSHIP_BRONZE, 'Bronze'),
-        (MEMBERSHIP_SILVER, 'Silver'),
-        (MEMBERSHIP_GOLD, 'Gold'),
-    ]
+    #MEMBERSHIP_CHOICES = [
+    #    (MEMBERSHIP_BRONZE, 'Bronze'),
+     #   (MEMBERSHIP_SILVER, 'Silver'),
+      #  (MEMBERSHIP_GOLD, 'Gold'),
+    #]
+    user_name = models.CharField(max_length=255,unique=True)
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
     email = models.EmailField(unique=True)
-    phone = models.CharField(max_length=255)
+    phone = models.CharField(max_length=10)
     birth_date = models.DateField(null=True, blank=True)
-    membership = models.CharField(
-        max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE)
+    # membership = models.CharField(
+    #    max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE)
 
     def __str__(self):
-        return f'{self.first_name} {self.last_name}'
+        return self.user_name
 
     class Meta:
-        ordering = ['first_name', 'last_name']
+        ordering = ['user_name']
 
+class Address(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    address = models.TextField(max_length=255)
+    town = models.CharField()
+    rue = models.CharField()
+    postal_code = models.IntegerField(max_length=5)
 
 class Order(models.Model):
     PAYMENT_STATUS_PENDING = 'P'
@@ -78,32 +94,69 @@ class Order(models.Model):
     placed_at = models.DateTimeField(auto_now_add=True)
     payment_status = models.CharField(
         max_length=1, choices=PAYMENT_STATUS_CHOICES, default=PAYMENT_STATUS_PENDING)
-    customer = models.ForeignKey(Customer, on_delete=models.PROTECT)
+    user = models.ForeignKey(User, on_delete=models.PROTECT)
 
 
 class OrderItem(models.Model):
     order = models.ForeignKey(Order, on_delete=models.PROTECT)
-    product = models.ForeignKey(Product, on_delete=models.PROTECT, related_name='orderitems')
-    quantity = models.PositiveSmallIntegerField()
-    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
+    Product_variation = models.ManyToManyField(Product_variation,null=True,on_delete=models.PROTECT,related_name='orderitems')
+    quantity = models.PositiveSmallIntegerField(default=0)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    date_added = models.DateTimeField(auto_now_add=True)
 
-
-class Address(models.Model):
-    street = models.CharField(max_length=255)
-    city = models.CharField(max_length=255)
-    customer = models.ForeignKey(
-        Customer, on_delete=models.CASCADE)
-
-
+class shipping(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    shipping_address = models.ForeignKey(Address,on_delete=models.CASCADE)
+    shipping_method = models.CharField(max_length=255)
+    shipping_cost = models.DecimalField(max_digits=10,decimal_places=2,null=True)
+    shipping_date = models.DateField(auto_now_add=True)
+    order = models.ForeignKey(Order,null=True,on_delete=models.CASCADE)
+    def __str__(self):
+        return "%s " % (self.user_name)
+    
 class Cart(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid4)
+    id = models.UUIDField(primary_key=True, default=uuid4) #did not understand this
     created_at = models.DateTimeField(auto_now_add=True)
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    total = models.DecimalField(max_digits=6, decimal_places=2)
 
 
 class CartItem(models.Model):
     cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name='items')
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    quantity = models.PositiveSmallIntegerField()
+    product_variation = models.ManyToManyField(Product_variation,null=True)
+    quantity = models.PositiveSmallIntegerField(default=0)
 
     class Meta:
-        unique_together = [['cart', 'product']]
+        unique_together = [['cart', 'product_variation']]
+
+class payement(models.Model):
+    order = models.ForeignKey(Order,null=True,on_delete=models.CASCADE)
+    payment_methode = models.CharField()
+    payment_date = models.DateField(auto_now_add=True)
+    payment_cost = models.DecimalField(max_digits=10,decimal_places=2,null=True)
+
+class review(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE,null=True)
+    review_date = models.DateField(auto_now_add=True)
+    comment = models.TextField(max_length=255)
+    product = models.ForeignKey(Product,on_delete=models.CASCADE, null=True)
+    rating = models.IntegerField(max_length=5)  #5 stars haka
+
+class favList(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE,null=True)
+    items = models.ForeignKey(Product,on_delete=models.CASCADE,null=True)
+    created_date = models.DateField(auto_now_add=True)
+
+class club(models.Model):
+    name = models.CharField(max_length=255)
+    discount_percentage = models.IntegerField()
+    level = models.CharField() 
+    #well hna the idea of club is not clear
+
+class club_member(models.Model):
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    club = models.ForeignKey(club,on_delete=models.CASCADE)
+    join_date = models.DateField()
+    expiry_date = models.DateField()
+    def __str__(self):
+        return "%s" % (self.user.user_name)
