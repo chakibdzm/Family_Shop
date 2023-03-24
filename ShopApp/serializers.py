1384b5bfc40ef5d4a1521a918b3b3f56c8549e87
SadekMaachou <ys.maachou@esi-sba.dz>
2023-03-24 11:52:54 +0100

    fixing everything

--- ShopApp/serializers.py
+++ ShopApp/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from decimal import Decimal
-from .models import Product, Collection, Cart
+from .models import Product, Collection, Cart, CartItem
 
 
 class CollectionSerializer(serializers.ModelSerializer):
@@ -8,14 +8,6 @@ class CollectionSerializer(serializers.ModelSerializer):
         model = Collection
         fields = ['id', 'title', 'products_count']
     products_count = serializers.IntegerField(read_only=True)
-
-
-class CartSerializer(serializers.ModelSerializer):
-    id = serializers.UUIDField(read_only=True)
-
-    class Meta:
-        model = Cart
-        fields = ['id']
 
 
 class ProductSerializer(serializers.ModelSerializer):
@@ -30,14 +22,68 @@ class ProductSerializer(serializers.ModelSerializer):
 
     def calculate_tax(self, product: Product):
         return product.unit_price * Decimal(1.1)
+    
 
-    def create(self, validated_data):
-        product = Product(**validated_data)  
-        product.other = 1
-        product.save()
-        return product
+class CartProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['id', 'title', 'unit_price']    
+      
 
-    def update(self, instance, validated_data):
-        instance.unit_price = validated_data.get('unit_price')
-        instance.save()
-        return instance      
+class CartItemSerializer(serializers.ModelSerializer):
+    product = CartProductSerializer()
+
+    class Meta:
+        model = CartItem
+        fields = ['id', 'product', 'quantity', 'total_price']
+    total_price = serializers.SerializerMethodField(method_name='calculate_price') 
+
+    def calculate_price(self, cart_item: CartItem):
+        return cart_item.product.unit_price * cart_item.quantity
+
+
+class CartSerializer(serializers.ModelSerializer):
+    id = serializers.UUIDField(read_only=True)
+    items = CartItemSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Cart
+        fields = ['id', 'items', 'total_price']
+    total_price = serializers.SerializerMethodField(method_name='calculate_total')   
+
+    def calculate_total(self, cart: Cart):
+        return sum([item.quantity * item.product.unit_price for item in cart.items.all()]) 
+
+
+class UpdateCartItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CartItem
+        fields = ['quantity']        
+
+
+class AddCartItemSerializer(serializers.ModelSerializer):
+    product_id = serializers.IntegerField()
+
+    def validate_product_id(self, value):
+        if not Product.objects.filter(pk=value).exists():
+            raise serializers.ValidationError('No product with the given ID was found.')
+        return value
+
+    def save(self, **kwargs):
+        cart_id = self.context['cart_id']
+        product_id = self.validated_data['product_id']
+        quantity = self.validated_data['quantity']
+
+        try:
+            cart_item = CartItem.objects.get(cart_id=cart_id, product_id=product_id)
+            cart_item.quantity += quantity
+            cart_item.save()
+            self.instance = cart_item
+        except CartItem.DoesNotExist:
+            self.instance = CartItem.objects.create(cart_id=cart_id, **self.validated_data)
+
+        return self.instance    
+
+    class Meta:
+        model = CartItem
+        fields = ['id', 'product_id', 'quantity']
--- ShopApp/urls.py
+++ ShopApp/urls.py
@@ -1,11 +1,14 @@
-from django.urls import path
 from . import views
 from rest_framework.routers import DefaultRouter
+from rest_framework_nested.routers import NestedDefaultRouter
 
 router = DefaultRouter()
 router.register('products', views.ProductViewSet)
 router.register('collections', views.CollectionViewSet)
-router.register('cart', views.CartViewSet)
+router.register('carts', views.CartViewSet)
+
+carts_router = NestedDefaultRouter(router, 'carts', lookup = 'cart')
+carts_router.register('items', views.CartItemViewSet, basename='cart-items')
 
 # URLConf
-urlpatterns = router.urls
+urlpatterns = router.urls + carts_router.urls
