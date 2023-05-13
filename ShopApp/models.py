54928bdff1f1c2cae96c06c3d052905f2a0b9541
oihid <c.ouchene@esi-sba.dz>
2023-05-13 16:41:28 +0100

    adding Auth To All fun

--- ShopApp/models.py
+++ ShopApp/models.py
@@ -95,6 +95,7 @@ class Comment(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     text = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
+    
 
     class Meta:
         ordering = ['-created_at']
--- ShopApp/urls.py
+++ ShopApp/urls.py
@@ -6,29 +6,22 @@ from rest_framework import routers
 from .views import *
 router = DefaultRouter()
 router.register('products', views.ProductViewSet)
-router.register('customers', views.CustomerViewSet)
 router.register('Categories', views.CollectionViewSet)
-router.register('carts', views.CartViewSet)
 router.register(r'clothes',views.ClotheViewSet)
+router.register('favorites', FavoriteViewSet,basename='favorites')
 
 
-
-carts_router = NestedDefaultRouter(router, 'carts', lookup = 'cart')
-carts_router.register('items', views.CartItemViewSet, basename='cart-items')
 
 
 # URLConf
-urlpatterns = router.urls+carts_router.urls+[
+urlpatterns = router.urls+[
     path('products/<int:product_id>',views.ProductDetail.as_view()),
     path('products/collection/<str:category_name>/', product_collection),
     path('clothes/collection/<str:category_name>/',views.ClotheViewSet.as_view({'get': 'clothes_collection'}), name='clothes-collection'),  
-    path('favorites/', FavoriteList.as_view(), name='favorite_list'),
-    path('favorites/<int:pk>/', FavoriteDetail.as_view(), name='favorite_detail'), 
-    path('cart_confirmed/<int:cart_id>/', views.OrderViewSet.as_view({'post': 'create'})), 
     path('comments/create/', CommentCreateAPIView.as_view(), name='comment-create'), 
     path('panier/', PanierItemList.as_view()),
     path('panier/add/', AddToPanier.as_view()),
-    path('panier/remove/<int:pk>/', RemoveFromPanier.as_view()),
+    path('panier/remove/<int:product_id>/', RemoveFromPanier.as_view()),
     path('ordering/', OrderView.as_view(), name='order-list'),
     path('order/', UserOrderListView.as_view(), name='user-order-list'),
     
