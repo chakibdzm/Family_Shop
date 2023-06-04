e00352f0f658b1a5996f76955f820d73ee56dc09
SadekMaachou <ys.maachou@esi-sba.dz>
2023-06-04 23:31:10 +0200

    reset password

--- FamilyShop/settings.py
+++ FamilyShop/settings.py
@@ -186,3 +186,9 @@ JWT_AUTH = {
 }
 
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'ys.maachou@gmail.com'
+EMAIL_HOST_PASSWORD = 'plrlcykblgnxtsud'
